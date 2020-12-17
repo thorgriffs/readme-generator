eb9903d2845e3b571e9b0616ba8beae7d83e5c81
thorgriffs <22.kelliking@gmail.com>
2020-12-17 10:43:24 -0500

    block quote formatting added to License

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -43,7 +43,7 @@ function generateMarkdown(data) {
   else if(data.License === 'GNU GPL v3') {
     license = createGnuLicense(data.Name);
   }
-  lines.push('>' + license);
+  lines.push(license);
 
 
   return lines.join('\r\n');
@@ -52,21 +52,21 @@ function generateMarkdown(data) {
 module.exports = generateMarkdown;
 
 function createMitLicense (name) {
-  return `MIT License
+  return `>MIT License
 
-  Copyright (c) 2020 `+name+`
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
+  >Copyright (c) 2020 `+name+`
+  >
+ > Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
   in the Software without restriction, including without limitation the rights
   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
-  
-  The above copyright notice and this permission notice shall be included in all
+  >
+  >The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
-  
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  >
+  >THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
