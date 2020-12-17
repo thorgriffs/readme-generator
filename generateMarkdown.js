b224cf0ba685d0b75db4263ec8d6d419c8a958c1
thorgriffs <22.kelliking@gmail.com>
2020-12-17 01:09:41 -0500

    adding additional license text to generateMarkdown.js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -20,8 +20,13 @@ function generateMarkdown(data) {
   if(data.License === 'MIT') {
     license = createMitLicense(data.Name);
   }
+  else if(data.License === 'Apache 2.0') {
+    license = createApacheLicense(data.Name);
+  }
+  else if(data.License === 'GNU GPL v3') {
+    license = createGnuLicense(data.Name);
+  }
   lines.push(license);
-
   lines.push('## Contributing');
   lines.push(data.Contributing);
   lines.push('## Tests');
@@ -58,4 +63,34 @@ function createMitLicense (name) {
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.`
-}
+};
+
+function createApacheLicense (name) {
+  return `Copyright 2020 `+name+`
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  
+      http://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.`
+};
+
+function createGnuLicense (name) {
+  return `Copyright (C) 2020  `+name+`
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.`
+};
